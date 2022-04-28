b38166eb938eb6b1170bfeb9c212bb38969ab6c3
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-28 12:29:18 -0500

    Cambio de enlaces de consumo al backend desplegado en heroku

--- src/services/transporteCargaAPI.js
+++ src/services/transporteCargaAPI.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 async function getAllVehicles(){
-    const response = await axios.get(`http://localhost:8080/vehiculos`);
+    const response = await axios.get(`https://transporte-carga-back.herokuapp.com/vehiculos`);
     return response.data;
 }
 
 async function postDriverInfo(info){
-    const response = await axios.post(`http://localhost:8080/vehiculo`,info);
+    const response = await axios.post(`https://transporte-carga-back.herokuapp.com/vehiculo`,info);
     return response.data.placa;
 }
 
 async function getDriverInfo(setCorreo){
-    const response = await axios.get(`http://localhost:8080/conductor/correo/${setCorreo}`);
+    const response = await axios.get(`https://transporte-carga-back.herokuapp.com/conductor/correo/${setCorreo}`);
     return response.data;
 }
 
